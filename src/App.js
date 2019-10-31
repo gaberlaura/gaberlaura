322101fcaaa652a1b1418c564fec72fd9f1f02f5
Admin <admin@Admins-Air.fios-router.home>
2019-10-31 16:02:40 -0400

    add resume & github link

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <div className="top-of-page">
         <header className="resume-header">
           <nav className="top-block">
-            <a className="resume-link">Resume</a>
+            <a className="resume-link" href='https://github.com/gaberlaura' target="_blank">Github</a>
+            <a className="resume-link" href='/LauraGaber.pdf' download>Resume</a>
             <a className="contact-link">Contact</a>
           </nav>
         </header>
@@ -17,10 +18,18 @@ function App() {
       </div>
       <body>
         <h3 className="brief-about">Front-end developer</h3>
-        <h3 className="brief-about">Years of professional experience: 4</h3>
-        <p>I am an experienced developer with a demonstrated history of working in the technology industry. 
+        <h3 className="brief-about">Education:   </h3>   
+        <span className="regular-text" >The University of Iowa, Iowa City, IA</span>                                                            
+        <h3 className="brief-about">Electrical & Computer Engineering  </h3>
+        <p className="regular-text">I am an experienced developer with a demonstrated history of working in the technology industry. 
           I am skilled in Javascript, HTML, CSS with a  
-          strong business background. </p>
+          strong business background. 
+        </p>
+        <h3 className="brief-about">Technical Skills:</h3>
+        Languages: HTML, CSS, JAVASCRIPT, EXTJS, C, C++, SQL, PL/SQL
+        Databases: Oracle, Cassandra, MySQL
+        Operating Systems: Mac, Linux, Windows
+        Development Tools: Git, Visual Studio Code, Putty, PL/SQL developer
       </body>
     </div>
   );
