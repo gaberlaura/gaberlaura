fc73fb99cd7c1345b6431b4efa554122c83f64dd
Admin <admin@Admins-Air.fios-router.home>
2019-10-31 15:28:02 -0400

    start main page

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="resume">
+      <div className="top-of-page">
+        <header className="resume-header">
+          <nav className="top-block">
+            <a className="resume-link">Resume</a>
+            <a className="contact-link">Contact</a>
+          </nav>
+        </header>
+        <img className="profile-img" src="profilePic.jpg" alt="profile image"></img>
+        <h2 className="name">Laura Gaber</h2>
+      </div>
+      <body>
+        <h3 className="brief-about">Front-end developer</h3>
+        <h3 className="brief-about">Years of professional experience: 4</h3>
+        <p>I am an experienced developer with a demonstrated history of working in the technology industry. 
+          I am skilled in Javascript, HTML, CSS with a  
+          strong business background. </p>
+      </body>
     </div>
   );
 }
