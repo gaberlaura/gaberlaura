c79b5e6d7a3d97d00ab8cde85edd80b0d43df7f4
Admin <admin@Admins-MacBook-Air.local>
2019-11-01 09:50:52 -0400

    style + resume info

--- src/App.js
+++ src/App.js
@@ -29,17 +29,76 @@ function App() {
                 <div>
                   <h3 className="brief-about">Front-end developer</h3>
                   <h3 className="brief-about">Education:   </h3>   
-                  <span className="regular-text" >The University of Iowa, Iowa City, IA</span>                                                            
-                  <h3 className="brief-about">Electrical & Computer Engineering  </h3>
+                  <span className="regular-text">Electrical & Computer Engineering  </span><br/> 
+                  <span className="regular-text" >The University of Iowa, Iowa City, IA</span>                                                           
+                  <h3 className="brief-about">About:   </h3>
                   <p className="regular-text">I am an experienced developer with a demonstrated history of working in the technology industry. 
                     I am skilled in Javascript, HTML, CSS with a  
                     strong business background. 
                   </p>
                   <h3 className="brief-about">Technical Skills:</h3>
-                  Languages: HTML, CSS, JAVASCRIPT, EXTJS, C, C++, SQL, PL/SQL
-                  Databases: Oracle, Cassandra, MySQL
-                  Operating Systems: Mac, Linux, Windows
-                  Development Tools: Git, Visual Studio Code, Putty, PL/SQL developer
+                  <div className="regular-text">
+                    <ul>
+                      <li>Languages: HTML, CSS, JAVASCRIPT, EXTJS, C, C++, SQL, PL/SQL</li>
+                      <li>Databases: Oracle, Cassandra, MySQL</li>
+                      <li>Operating Systems: Mac, Linux, Windows</li>
+                      <li>Development Tools: Git, Visual Studio Code, Putty, PL/SQL developer</li>
+                    </ul>
+                  </div>
+                  <h3 className="brief-about">Certifications: </h3>
+                  <div className="regular-text">
+                    <ul>
+                      <li>Oracle Database 12C: Advanced PL/SQL</li>
+                    </ul>
+                  </div>
+                  <h3 className="brief-about">Language Proficiencies: </h3>
+                  <div className="regular-text">
+                    <ul>
+                      <li>Fluent in English and Arabic</li>
+                    </ul>
+                  </div>
+                  <h3 className="brief-about">Professional Experience: </h3>
+                  <div className="regular-text">
+                    <span className="job-title">Founder, </span>
+                    <span> Five Stars Tutoring</span><br/>
+                    <span className="job-title">Software engineer, </span>
+                    <span> Novetta</span><br/>
+                    <span className="job-title">Software engineer, </span>
+                    <span> General Dynamics IT</span><br/>
+                  </div>
+                  <h3 className="brief-about">Internships/Student Experience: </h3>
+                  <div className="regular-text">
+                    <span className="job-title">Student developer, </span>
+                    <span> University of Iowa Biology Department</span><br/>
+                    <span className="job-title">Database intern, </span>
+                    <span> General Dynamics</span><br/>
+                    <span className="job-title">Intern, </span>
+                    <span> Emerson Process Management</span><br/>
+                    <span className="job-title">Database developer, </span>
+                    <span> University of Iowa Hospital</span><br/>
+                    <span className="job-title">Tech lab assistant, </span>
+                    <span> University of Iowa Medical Research Center</span><br/>
+                  </div>
+                  <h3 className="brief-about">Tutoring Experience: </h3>
+                  <div className="regular-text">
+                    <span className="job-title">Tutor, </span>
+                    <span> Five Stars Tutoring</span><br/>
+                    <span className="job-title">Tutor, </span>
+                    <span> Bullseye Learning</span><br/>
+                    <span className="job-title">Tutor, </span>
+                    <span> Youth Lift</span><br/>
+                    <span className="job-title">Tutor, </span>
+                    <span> University of Iowa</span><br/>
+                    <span className="job-title">Tutor, </span>
+                    <span> Eye Level Learning</span><br/>
+                  </div>
+                  <h3 className="brief-about">Leadership Experience: </h3>
+                  <div className="regular-text">
+                    <span className="job-title">President, </span>
+                    <span> Engineering Student Counsil</span><br/>
+                    <span className="job-title">Mentor/Ambassador, </span>
+                    <span> Women in Science and Engineering</span><br/>
+                  </div>
               </div>
           );
         }}/>
--- src/Contact.js
+++ src/Contact.js
@@ -4,11 +4,15 @@ class Contact extends React.Component{
     render(){
         return(
             <form action="/action_page.php" className="contact-form">
-                Full name:<br/>
+                Name:<br/>
                 <input type="text" name="fullname"/>
-                <br/>
+                <br/><br/>
+                Today's date:
+                <input type="date"/><br/><br/>
                 Email:<br/>
-                <input type="text" name="email"/>
+                <input type="text" name="email"/><br/><br/>
+                Message:<br/>
+                <textarea name="message" rows="10" cols="30"></textarea>
                 <br/><br/>
                 <input type="submit" value="Submit"/>
             </form>
